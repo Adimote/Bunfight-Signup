6785cc3310407e249a2da647632049a543515b11
Andrew Barrett-Sprot <abs1g14@soton.ac.uk>
2015-09-24 10:37:57 +0100

    Working Example

--- server.py
+++ server.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def sign_up():
     return render_template('sign_up.html')
+
+
+@app.route('/signed_up', methods=['POST'])
+def signed_up():
+    with open('signups.txt', 'a') as f:
+        name = request.form['name']
+        email = request.form['email']
+        f.write("{},{}\n".format(name, email))
+    return "", 201
 
 if __name__ == '__main__':
     app.debug = True
